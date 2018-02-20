96343e8fcea991c3b2eeb1507e76ffecab0e7138
Uyiosa-Oriakhi <mark.oriakhi@gmail.com>
2018-02-20 12:36:55 -0500

    Updated exercise file(s)

--- Uyiosa-Oriakhi/exercise.js
+++ Uyiosa-Oriakhi/exercise.js
@@ -6,12 +6,25 @@ exercise.salary = null;
 // load salary and data
 exercise.load = function(salary, boston){
     exercise.salaries = boston.data;
-    exercise.salary = salary;
+    exercise.salary = 99000;
 };
 
 // get salaries larger than given salary
 exercise.findBiggerSalaries = function(){
+    var num_salaries = 0;
+    var dataLength = exercise.salaries.length;
+    var currentSal = 0;
+    var list =[]
+    for (var i=0; i<dataLength; i++){
+        var currentSalary = exercise.salaries[i][18]
+        if (Number(exercise.salaries[i][18])>exercise.salary){
 
+            list.push(currentSalary)
+        }
+    }
+    console.log(list)
+    return list 
+    
     // ---------------------------------------------------
     //   YOUR CODE
     //
@@ -26,8 +39,18 @@ exercise.findBiggerSalaries = function(){
 // filter function
 exercise.largerSalary = function largerSalary(item){
 
-    // ---------------------------------------------------
-    //   YOUR CODE
+    var num_salaries = 0;
+    var dataLength = exercise.salaries.length;
+    var currentSal = 0;
+    var listt =[]
+    for (var i=0; i<dataLength; i++){
+        
+
+            listt.push(Number(exercise.salaries[i][18])>exercise.salary)
+        } // ---------------------------------------------------
+    
+    console.log(listt)
+    return listt    //   YOUR CODE
     //
     //     Return a boolean (true/false) if item
     //     is larger than exercise.salary
